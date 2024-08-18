a218b736520c3049230b9c3d1b032e3a820ffe7b
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-08-19 00:56:52 +0300

    Update PostJob.jsx

--- job-portal-client/src/pages/PostJob.jsx
+++ job-portal-client/src/pages/PostJob.jsx
@@ -10,7 +10,7 @@ const PostJob = () => {
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const BASE_URL = 'http://localhost:7777/api/jobs';
+    const BASE_URL = 'https://techposter-backend.onrender.com/api/jobs';
 
     const { user } = useAuthContext();
 
